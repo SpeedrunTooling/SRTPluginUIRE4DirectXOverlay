fe788c5b5101c8aaf3d1179e8b15d9a971daa05c
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2023-05-26 23:37:00 -0500

    PluginInfo

--- SRTPluginUIRE4DirectXOverlay/PluginInfo.cs
+++ SRTPluginUIRE4DirectXOverlay/PluginInfo.cs
@@ -3,25 +3,22 @@ using System;
 
 namespace SRTPluginUIRE4DirectXOverlay
 {
-    internal class PluginInfo : IPluginInfo
+    internal class PluginInfo : PluginInfoBase, IPluginInfo
     {
-        public string Name => "DirectX Overlay UI (Resident Evil 4 Remake (2023))";
+        public override string Name => "DirectX Overlay UI (Resident Evil 4 Remake (2023))";
 
-        public string Description => "A DirectX-based Overlay User Interface for displaying Resident Evil 4 Remake (2023) UI. (SRT Host 4.0)";
+        public override string Description => "A DirectX-based Overlay User Interface for displaying Resident Evil 4 Remake (2023) UI. (SRT Host 4.0)";
 
-        public string Author => "VideoGameRoulette & Squirrelies";
+        public override string Author => "VideoGameRoulette & Squirrelies";
 
-        public Uri MoreInfoURL => new Uri("https://github.com/SpeedrunTooling/SRTPluginUIRE4DirectXOverlay");
+        public override Uri MoreInfoURL => new Uri("https://github.com/SpeedrunTooling/SRTPluginUIRE4DirectXOverlay");
 
-		public int VersionMajor => assemblyVersion?.Major ?? 0;
+        public override int VersionMajor => GetProductVersion().Major;
 
-		public int VersionMinor => assemblyVersion?.Minor ?? 0;
+        public override int VersionMinor => GetProductVersion().Minor;
 
-		public int VersionBuild => assemblyVersion?.Build ?? 0;
+        public override int VersionBuild => GetProductVersion().Build;
 
-		public int VersionRevision => assemblyVersion?.Revision ?? 0;
-
-		private readonly Version? assemblyVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
-		public bool Equals(IPluginInfo? other) => Equals(this, other);
-	}
+        public override int VersionRevision => GetProductVersion().Revision;
+    }
 }
